3c09b09ace3247e5fdf862d62ff1c503cacd81c3
jasonhadddad <jason.alhaddad@outlook.com>
2016-10-09 19:14:36 +1100

    Add files via upload
    as of 7pm sunday

---
+++ iRobot_Maze.X/motor.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   motor.h
+ * Author: Brendan
+ *
+ * Created on 24 September 2016, 1:15 PM
+ */
+
+#ifndef MOTOR_H
+#define	MOTOR_H
+
+void stepCW(int steps);
+void stepCCW(int steps);
+
+int loop = 0;
+
+#endif	/* MOTOR_H */
+
